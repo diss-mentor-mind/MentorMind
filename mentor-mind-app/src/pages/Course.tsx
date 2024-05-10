78cb9b3f964f2a052f93cd278a3b89240978b158
913-Iulia-Ioana-Farcas <72136515+iuliaafarcas@users.noreply.github.com>
2024-05-10 17:43:46 +0300

    done with filters

--- mentor-mind-app/src/pages/Course.tsx
+++ mentor-mind-app/src/pages/Course.tsx
@@ -23,161 +23,166 @@ const Course = () => {
   const [statusC, setStatusC] = useState("Active");
 
   return (
-    <Grid sx={{ display: "flex", flexDirection: "row" }}>
-      <Grid
-        container
-        direction="column"
-        sx={{
-          backgroundColor: "var(--primary-color);",
-          marginTop: "2%",
-          marginLeft: "1.5%",
-          width: "15%",
-          height: "550px",
-          boxShadow: "5px 5px 10px rgb(0 0 0 / 0.2)",
-        }}
-      >
-        <Typography
+    <Grid
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        paddingTop: "1%",
+        height: "85%",
+      }}
+    >
+      <PinnedContainer width="15%" height="98%">
+        <Grid
+          container
+          direction="column"
           sx={{
-            color: "white",
-            fontSize: "x-large",
-            textAlign: "center",
-            paddingTop: "5%",
-          }}
-        >
-          Course name
-        </Typography>
-
-        <Typography
-          sx={{
-            color: "white",
-            fontSize: "large",
-            textAlign: "left",
-            paddingLeft: "10%",
-            paddingTop: "10%",
-          }}
-        >
-          Filters
-        </Typography>
-
-        <TextField
-          value={title}
-          InputProps={{
-            sx: {
-              color: "white", // Set the color of the input text (including value) to white
-            },
-          }}
-          sx={{
-            marginTop: "10%",
-            color: "white",
-            placeholder: "Search by title",
-
-            marginLeft: "5%",
-            width: "90%",
-            boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.5)",
-          }}
-        ></TextField>
-        <TextField
-          value={publiser}
-          InputProps={{
-            sx: {
-              color: "white", // Set the color of the input text (including value) to white
-            },
-          }}
-          sx={{
-            color: "white",
-            marginTop: "10%",
-            marginLeft: "5%",
-            width: "90%",
-
-            boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.5)",
-          }}
-        ></TextField>
-
-        <FormControl
-          sx={{
-            boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.5)",
-            marginTop: "10%",
-            width: "90%",
-            marginLeft: "5%",
-          }}
-        >
-          <InputLabel
-            id="demo-simple-select-label"
-            sx={{
-              color: "white",
-              "&.Mui-focused": {
-                // Apply styles when the input label is focused
-                color: "white",
-              },
-            }}
-          >
-            File Type
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            label="Age"
-          >
-            <MenuItem value={10}>File</MenuItem>
-            <MenuItem value={20}>Video</MenuItem>
-            <MenuItem value={30}>Archive</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl
-          sx={{
-            boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.5)",
-            marginTop: "10%",
-            width: "90%",
-            marginLeft: "5%",
-          }}
-        >
-          <InputLabel
-            sx={{
-              color: "white",
-              "&.Mui-focused": {
-                // Apply styles when the input label is focused
-                color: "white",
-              },
-            }}
-            id="demo-simple-select-label"
-          >
-            Active
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            sx={{
-              "& .MuiSelect-select.MuiSelect-selectMenu": {
-                color: "white", // Set the color of the selected value to white
-              },
-            }}
-          >
-            <MenuItem value={10}>Active</MenuItem>
-            <MenuItem value={20}>In review</MenuItem>
-          </Select>
-        </FormControl>
-
-        {/* Adjusted button position within the grid */}
-        <Box
-          sx={{
+            marginTop: "2%",
+            marginLeft: "1.5%",
+            width: "100%",
+            height: "100%",
             display: "flex",
-            justifyContent: "center",
-            marginTop: "auto",
-            marginBottom: "2%",
+            flexDirection: "column",
           }}
         >
-          <Button
+          <Typography
             sx={{
-              backgroundColor: "var(--button-color)",
               color: "white",
-              width: "90%",
+              fontSize: "x-large",
+              textAlign: "center",
+              paddingTop: "5%",
+            }}
+          >
+            Course name
+          </Typography>
+
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "large",
+              textAlign: "left",
+              paddingLeft: "5%",
+              paddingTop: "10%",
+            }}
+          >
+            Filters
+          </Typography>
+
+          <TextField
+            value={title}
+            InputProps={{
+              sx: {
+                color: "white", // Set the color of the input text (including value) to white
+              },
+            }}
+            sx={{
+              marginTop: "10%",
+              color: "white",
+              placeholder: "Search by title",
+              width: "100%",
+              boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.5)",
+            }}
+          ></TextField>
+          <TextField
+            value={publiser}
+            InputProps={{
+              sx: {
+                color: "white", // Set the color of the input text (including value) to white
+              },
+            }}
+            sx={{
+              color: "white",
+              marginTop: "10%",
+              width: "100%",
+
+              boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.5)",
+            }}
+          ></TextField>
+
+          <FormControl
+            sx={{
+              boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.5)",
+              marginTop: "10%",
+              width: "100%",
+            }}
+          >
+            <InputLabel
+              id="demo-simple-select-label"
+              sx={{
+                color: "white",
+                "&.Mui-focused": {
+                  // Apply styles when the input label is focused
+                  color: "white",
+                },
+              }}
+            >
+              File Type
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Age"
+            >
+              <MenuItem value={10}>File</MenuItem>
+              <MenuItem value={20}>Video</MenuItem>
+              <MenuItem value={30}>Archive</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl
+            sx={{
+              boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.5)",
+              marginTop: "10%",
+              width: "100%",
+            }}
+          >
+            <InputLabel
+              sx={{
+                color: "white",
+                "&.Mui-focused": {
+                  // Apply styles when the input label is focused
+                  color: "white",
+                },
+              }}
+              id="demo-simple-select-label"
+            >
+              Active
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              sx={{
+                "& .MuiSelect-select.MuiSelect-selectMenu": {
+                  color: "white", // Set the color of the selected value to white
+                },
+              }}
+            >
+              <MenuItem value={10}>Active</MenuItem>
+              <MenuItem value={20}>In review</MenuItem>
+            </Select>
+          </FormControl>
+
+          {/* Adjusted button position within the grid */}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "auto",
               marginBottom: "10%",
             }}
           >
-            Manage Course
-          </Button>
-        </Box>
-      </Grid>
+            <Button
+              sx={{
+                backgroundColor: "var(--button-color)",
+                color: "white",
+                width: "100%",
+                height: "90%",
+                marginBottom: "10%",
+              }}
+            >
+              Manage Course
+            </Button>
+          </Box>
+        </Grid>
+      </PinnedContainer>
       <Grid
         sx={{
           width: "75%",
