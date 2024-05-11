54383eb80b2e50e954636c747101c0e8a735c06c
Liviu-Terebes <tlie3028@scs.ubbcluj.ro>
2024-05-11 18:27:51 +0300

    Add + button and formatting

--- mentor-mind-app/src/pages/CoursesPage.tsx
+++ mentor-mind-app/src/pages/CoursesPage.tsx
@@ -1,33 +1,68 @@
-import { Grid } from "@mui/material";
+import { Fab, Grid } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import SubjectContainer from "../components/containers/SubjectContainer";
-import SubjectInterface, { populatedSubjects } from "../interfaces/SubjectInterface";
+import SubjectInterface, {
+  populatedSubjects,
+} from "../interfaces/SubjectInterface";
 
 const CoursePage = () => {
-    const courses: SubjectInterface[] = populatedSubjects;
+  const courses: SubjectInterface[] = populatedSubjects;
 
-    return (
-        <Grid sx={{
-            marginTop: "2%",
-            marginLeft: "13%", // Center horizontally
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            alignContent: "flex-start",
-        }}>
-            {courses.map((course, index) => (
-                <Grid
-                    key={index}
-                    sx={{
-                        width: "20%",
-                        height: "280px",
-                        marginBottom: "2%",
-                        ...(index % 3 !== 2 && { marginRight: "12%" }), // Apply right margin except for the last item in each row
-                    }}>
-                    <SubjectContainer {...course} />
-                </Grid>
-            ))}
+  const handleAddCourse = () => {
+    console.log("Add course clicked!");
+  };
+
+  return (
+    <Grid
+      sx={{
+        marginTop: "2%",
+        marginLeft: "13%", // Center horizontally
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignContent: "flex-start",
+      }}
+    >
+      {courses.map((course, index) => (
+        <Grid
+          key={index}
+          sx={{
+            width: "20%",
+            height: "280px",
+            marginBottom: "2%",
+            ...(index % 3 !== 2 && { marginRight: "12%" }), // Apply right margin except for the last item in each row
+          }}
+        >
+          <SubjectContainer {...course} />
         </Grid>
-    );
+      ))}
+
+      <Fab
+        aria-label="add"
+        onClick={handleAddCourse}
+        sx={{
+          position: "absolute",
+          bottom: "16px",
+          right: "16px",
+          color: "var(--icon-primary-color)",
+          background: "var(--button-color)",
+          width: "70px",
+          height: "70px",
+          borderRadius: "50%",
+          "&:hover": {
+            background: "var(--dark-button-color)",
+          },
+          "& svg": {
+            // Apply styles to the svg icon inside the button
+            width: "60px",
+            height: "60px",
+          },
+        }}
+      >
+        <AddIcon />
+      </Fab>
+    </Grid>
+  );
 };
 
 export default CoursePage;
