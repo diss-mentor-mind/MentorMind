ada6bd2de74d1ecefa6a30faf25c85fb8494d714
Dubau-Horea-Filip <filipdubau@yahoo.ro>
2024-05-13 19:00:22 +0300

    scrolable

--- mentor-mind-app/src/pages/VideoPage.tsx
+++ mentor-mind-app/src/pages/VideoPage.tsx
@@ -1,110 +1,142 @@
-import { Box, Grid, Button, Container } from "@mui/material";
+import { Box, Grid, Button } from "@mui/material";
 import Comments from "../components/comments/comments";
 import PinnedContainer from "../components/containers/PinnedContainer";
 
-
-
-
 const VideoPage = () => {
   return (
-    <Grid container spacing={2} sx={{
-      display: "flex",
-      flexDirection: "row"
-
-    }} >  { }
-      <Grid sx={{
-        height: "700px",
-        width: "1000px",
-        marginLeft: "40px",
-        marginTop: "50px"
-      }}> { }
-        <PinnedContainer
-          height="100%"
-          width="100%" > { }
+    <Grid container spacing={2} sx={{ display: "flex", flexDirection: "row" }}>
+      <Grid sx={{ height: "700px", width: "1000px", marginLeft: "40px", marginTop: "50px" }}>
+        <PinnedContainer height="100%" width="100%">
           {"Video here"}
         </PinnedContainer>
       </Grid>
-      <Grid
-        sx={{
-          height: "100%",
-          width: "20%",
-          marginLeft: "40px",
-          marginTop: "50px"
-        }}
-      >
-        <PinnedContainer
-          height="100%"
-          width="100%"
-        >
-          { }
-          <Comments
-    comments={[
-        {
-            id: '1',
-            username: 'John Doe',
-            text: 'This is a great article!',
-            timestamp: '2024-05-11T15:47:00',
-            profilePicture: null,
-            replies: [
+      <Grid sx={{ height: "100%", width: "20%", marginLeft: "40px", marginTop: "50px" }}>
+        <PinnedContainer height="100%" width="100%">
+          <Box
+            sx={{
+              height: "500px",
+              overflowY: "auto",
+              scrollbarWidth: "thin",
+              "&::-webkit-scrollbar": {
+                width: "8px",
+              },
+              "&::-webkit-scrollbar-thumb": {
+                backgroundColor: "#888",
+                borderRadius: "4px",
+              },
+              "&::-webkit-scrollbar-track:hover": {
+                backgroundColor: "#ddd",
+              },
+              "&::-webkit-scrollbar-thumb:hover": {
+                backgroundColor: "#666",
+              },
+            }}
+          > {/* Make the box scrollable */}
+            <Comments
+              comments={[
                 {
-                    id: '1-1',
-                    username: 'Alice',
-                    text: ' REPLY I agree!',
-                    timestamp: '2024-05-11T15:49:00',
-                    profilePicture: null
-                }
-            ]
-        },
-        {
-            id: '2',
-            username: 'Jane Smith',
-            text: 'I agree, very informative!',
-            timestamp: '2024-05-11T15:48:00',
-            profilePicture: null,
-            replies: [
+                  id: '1',
+                  username: 'John Doe',
+                  text: 'This is a great article!',
+                  timestamp: '2024-05-11T15:47:00',
+                  profilePicture: null,
+                  replies: [
+                    {
+                      id: '1-1',
+                      username: 'Alice',
+                      text: ' REPLY I agree!',
+                      timestamp: '2024-05-11T15:49:00',
+                      profilePicture: null
+                    }
+                  ]
+                },
                 {
-                    id: '2-1',
-                    username: 'Bob',
-                    text: 'REPLY I disagree!',
-                    timestamp: '2024-05-11T15:50:00',
-                    profilePicture: null
-                }
-            ]
-        },
-        {
-            id: '3',
-            username: 'Alice Johnson',
-            text: 'Thanks for sharing!',
-            timestamp: '2024-05-11T15:49:00',
-            profilePicture: null,
-            replies: [
+                  id: '2',
+                  username: 'Jane Smith',
+                  text: 'I agree, very informative!',
+                  timestamp: '2024-05-11T15:48:00',
+                  profilePicture: null,
+                  replies: [
+                    {
+                      id: '2-1',
+                      username: 'Bob',
+                      text: 'REPLY I disagree!',
+                      timestamp: '2024-05-11T15:50:00',
+                      profilePicture: null
+                    }
+                  ]
+                },
                 {
-                    id: '3-1',
-                    username: 'John Doe',
-                    text: 'REPLY You\'re welcome!',
-                    timestamp: '2024-05-11T15:51:00',
-                    profilePicture: null
-                }
-            ]
-        },
-    ]}
-/>
-
-          <div style={{ marginTop: '20px' }}> { }
+                  id: '3',
+                  username: 'Alice Johnson',
+                  text: 'Thanks for sharing!',
+                  timestamp: '2024-05-11T15:49:00',
+                  profilePicture: null,
+                  replies: [
+                    {
+                      id: '3-1',
+                      username: 'John Doe',
+                      text: 'REPLY You\'re welcome!',
+                      timestamp: '2024-05-11T15:51:00',
+                      profilePicture: null
+                    }
+                  ]
+                }, {
+                  id: '3',
+                  username: 'Alice Johnson',
+                  text: 'Thanks for sharing!',
+                  timestamp: '2024-05-11T15:49:00',
+                  profilePicture: null,
+                  replies: [
+                    {
+                      id: '3-1',
+                      username: 'John Doe',
+                      text: 'REPLY You\'re welcome!',
+                      timestamp: '2024-05-11T15:51:00',
+                      profilePicture: null
+                    }
+                  ]
+                }, {
+                  id: '3',
+                  username: 'Alice Johnson',
+                  text: 'Thanks for sharing!',
+                  timestamp: '2024-05-11T15:49:00',
+                  profilePicture: null,
+                  replies: [
+                    {
+                      id: '3-1',
+                      username: 'John Doe',
+                      text: 'REPLY You\'re welcome!',
+                      timestamp: '2024-05-11T15:51:00',
+                      profilePicture: null
+                    }
+                  ]
+                }, {
+                  id: '3',
+                  username: 'Alice Johnson',
+                  text: 'Thanks for sharing!',
+                  timestamp: '2024-05-11T15:49:00',
+                  profilePicture: null,
+                  replies: [
+                    {
+                      id: '3-1',
+                      username: 'John Doe',
+                      text: 'REPLY You\'re welcome!',
+                      timestamp: '2024-05-11T15:51:00',
+                      profilePicture: null
+                    }
+                  ]
+                },
+              ]}
+            />
+          </Box>
+          <div style={{ marginTop: '20px' }}>
             <label style={{ display: 'flex', alignItems: 'center' }}>
-              <input
-                type="checkbox"
-                // checked={includeTimestamp}
-                // onChange={handleCheckboxChange}
-                style={{ marginRight: '5px' }}
-              />
+              <input type="checkbox" style={{ marginRight: '5px' }} />
               <span style={{ fontSize: '14px' }}>Tag current timestamp</span>
             </label>
-
             <input
               type="text"
-              // value={newComment}
-              // onChange={handleCommentChange}
               placeholder="Add a comment..."
               style={{
                 marginRight: '10px',
@@ -116,10 +148,8 @@ const VideoPage = () => {
                 fontSize: '14px',
                 boxSizing: 'border-box',
                 marginTop: '10px',
-
               }}
             />
-
             <Button
               sx={{
                 backgroundColor: "var(--button-color)",
@@ -131,9 +161,10 @@ const VideoPage = () => {
               }}
             >Add Comment</Button>
           </div>
-        </PinnedContainer></Grid>
-
+        </PinnedContainer>
+      </Grid>
     </Grid>
   );
 };
+
 export default VideoPage;
